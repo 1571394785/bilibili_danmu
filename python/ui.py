e5c3e3b1a8bd4c88919865967b97625b28dd503f
乐乐 <1571394785@qq.com>
2023-01-28 21:44:06 +0800

    1

--- python/ui.py
+++ python/ui.py
@@ -2,20 +2,28 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
+import start,time
 def js_callback(result):
     print(result)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("PyQt5 WebEngine")
+        self.setWindowTitle("哔哩哔哩弹幕姬")
         self.setGeometry(100, 100, 800, 600)
         self.browser = QWebEngineView()
-        self.browser.load(QUrl("http://121.43.33.137"))
-        self.browser.page().runJavaScript("documet.getElementById('nav')", js_callback)
+        self.button=QPushButton('搜索',self)
+        html = open('python/ui/index.html', 'r', encoding='utf-8').read()
+        
+        js_string='''
+        
+        run();
+        '''
+        #等待加载完毕
+        self.browser.setHtml(html)
+        self.browser.loadFinished.connect(lambda: self.browser.page().runJavaScript(js_string, js_callback))
+        
         self.setCentralWidget(self.browser)
-    def bs(self):
-        return self.browser
 app = QApplication([])
 win = MainWindow()
 win.show()
